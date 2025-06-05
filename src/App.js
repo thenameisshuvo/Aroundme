bacda22fe8d0a27b03c80871c2eb4ca000997fe0
Tanvir Rahman Shuvo <shuvotanvir702@gmail.com>
2025-06-05 23:10:06 +0600

    Add initial project setup with environment configuration, routing, and styling

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Banner from "./components/Banner/Banner";
+import AppRoutes from "./routes";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+
+const App = () => {
+  return (
+    <div>
+      <Banner
+        bannerTitle={"Big news!"}
+        bannerDescription={"Introducing Android version of app."}
+        bannerButtonText={"Install app"}
+        bannerPlace={"top"}
+        bannerButtonUrl={"https://github.com/thenameisshuvo"}
+      />
+      <AppRoutes />
+      <ScrollToTop />
+    </div>
+  );
+};
+
+export default App;
